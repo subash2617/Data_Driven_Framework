c1104a580c1f943b09d304526083afcb51051474
Subash_S <74961842+subash2617@users.noreply.github.com>
2021-09-22 19:57:57 +0530

    Update ExcelRead2.java

--- src/test/java/dataDrivenFramework/ExcelRead2.java
+++ src/test/java/dataDrivenFramework/ExcelRead2.java
@@ -9,6 +9,7 @@ public class ExcelRead2 extends LibGlobal{
 	
 	public static void main(String[] args) throws IOException {
 		
+		//getData methods are in com.utils >> LibGlobal Lib_global file
 		System.out.println(getData(2, 0));
 		System.out.println(getData(2, 1));
 		System.out.println(getData(2, 2));
