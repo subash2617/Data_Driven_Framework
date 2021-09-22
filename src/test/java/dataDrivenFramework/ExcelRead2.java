88dd0a82e2c5b7452bcb8c44b5c3065160ca9820
Subash_S <74961842+subash2617@users.noreply.github.com>
2021-09-22 19:51:49 +0530

    Add files via upload

---
+++ src/test/java/dataDrivenFramework/ExcelRead2.java
@@ -0,0 +1,16 @@
+package dataDrivenFramework;
+
+import java.io.IOException;
+
+import com.utils.LibGlobal;
+
+public class ExcelRead2 extends LibGlobal{
+
+	
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println(getData(2, 0));
+		System.out.println(getData(2, 1));
+		System.out.println(getData(2, 2));
+	}
+}
