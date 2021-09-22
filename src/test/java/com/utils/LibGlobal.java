8f475841fc32c7c3b4b0ff01e70cd6f6da570462
Subash_S <74961842+subash2617@users.noreply.github.com>
2021-09-22 19:55:29 +0530

    Update LibGlobal.java
    DataDriven = Excelreader advance to ExcelReader2
    inside the DataDrivenFramework folder

--- src/test/java/com/utils/LibGlobal.java
+++ src/test/java/com/utils/LibGlobal.java
@@ -27,4 +27,31 @@ public class LibGlobal {
 	public static void quitBrowser() {
 		driver.quit();
 	}
+	
+	public static String getData(int row, int cell) throws IOException {
+		File loc = new File("C:\\Users\\SUBASH\\eclipse-workspace\\SeleniumFrameworks\\Input\\TestData.xlsx");
+		FileInputStream f = new FileInputStream(loc);
+		Workbook w = new XSSFWorkbook(f);
+		Sheet s = w.getSheet("Sheet1");
+		Row r = s.getRow(row);		
+		Cell c = r.getCell(cell);
+	
+		String value = null;
+		//CellType 1-->String, 0-->Number
+		int cellType = c.getCellType();
+		if (cellType == 1) {
+			value = c.getStringCellValue();
+		} else if (cellType == 0) {
+			if (DateUtil.isCellDateFormatted(c)) {
+				Date dateCellValue = c.getDateCellValue();
+				SimpleDateFormat sim = new SimpleDateFormat("dd/MM/YYYY");
+				value = sim.format(dateCellValue);
+			}else {
+				double numericCellValue = c.getNumericCellValue();
+				long l = (long) numericCellValue;
+				value = String.valueOf(l);
+			}
+		}
+		return value;
+	}
 }
